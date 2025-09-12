eee9de6e266e56e0d7543c5c3b977bbb2733cad8
Victor Baitsumedi <baitsumedi.pv@gmail.com>
2025-09-12 07:57:32 +0200

    Added a new file 2-arguments.js
    Created a code that checks for arguments.

---
+++ 2-arguments.js
@@ -0,0 +1,17 @@
+const myArgument = process.argv[2];
+
+if (myArgument === undefined) {
+  console.log('No argument');
+
+} else if (myArgument=="Best"){
+console.log("Argument found");
+}
+  
+else if (myArguement=="Best School"){
+  cosole.log("Arguments found");
+}
+
+else
+  {
+  console.log('Arguments found');
+}
