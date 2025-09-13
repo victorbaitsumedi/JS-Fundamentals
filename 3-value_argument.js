1fffb788a914e65d15ba9f6dbb7bd8d9bb1085e0
victorbaitsumedi <baitsumedi.pv@gmail.com>
2025-09-13 11:42:53 +0200

    added a new file contaisn a script that prints the first node argument passed to it

---
+++ 3-value_argument.js
@@ -0,0 +1,23 @@
+
+/*
+
+Write a script that prints the first argument passed to it:
+
+If no arguments are passed to the script, print “No argument”
+You must use console.log(...) to print all output
+You are not allowed to use var
+You are not allowed to use length
+
+
+*/
+
+const firstArgument = process.argv[2];
+
+if (firstArgument === undefined){
+console.log("No argument");
+}
+
+else{
+    console.log(firstArgument);
+}
+
