57fd524e2cd87588f9e84cf017516bcff9055c83
victorbaitsumedi <baitsumedi.pv@gmail.com>
2025-09-13 14:13:09 +0200

    updated the code

--- 8-square.js
+++ 8-square.js
@@ -1,20 +1,12 @@
-/*Write a script that prints a square
+const size = parseInt(process.argv[2]);
 
-The first argument is the size of the square
-If the first argument can’t be converted to an integer, print “Missing size”
-You must use the character X to print the square
-You must use console.log(...) to print all output
-You are not allowed to use var
-You must use a loop (while, for, etc.)
-*/
-
-const square = parseInt(process.argv[2]);
-
-if (isNaN(square)) {
+if (isNaN(size)) {
     console.log("Missing size");
 } else {
-    const row = "X".repeat(square);
-    for (let i = 0; i < square; i++) {
+    const row = "X".repeat(size);
+    for (let i = 0; i < size; i++) {
         console.log(row);
     }
 }
+
+
