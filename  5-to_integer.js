27b2ee83fedd0b9174c31a480c18a5733e08f422
victorbaitsumedi <baitsumedi.pv@gmail.com>
2025-09-13 12:44:46 +0200

    Wrote a scrip that converts the passed argument to an interger.

---
+++  5-to_integer.js
@@ -0,0 +1,18 @@
+/*
+Write a script that prints My number: <first argument converted in integer> if the first argument can be converted to an integer:
+
+If the argument can’t be converted to an integer, print “Not a number”
+You must use console.log(...) to print all output
+You are not allowed to use var
+You are not allowed to use try/catch
+*/
+
+
+const convert_to_interger = process.argv[2];  // first argument as string
+const myNumber = parseInt(convert_to_interger); // convert to integer
+
+if (Number.isNaN(myNumber)) {
+  console.log("Not a number");
+} else {
+  console.log(`My number: ${myNumber}`);
+}
