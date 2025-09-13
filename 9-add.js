687555fe3cce7d4e8b932702ebb23c9f4308e104
victorbaitsumedi <baitsumedi.pv@gmail.com>
2025-09-13 14:23:28 +0200

    Wrote a script that prints the addition of 2 integers

---
+++ 9-add.js
@@ -0,0 +1,21 @@
+/*
+
+Write a script that prints the addition of 2 integers
+
+The first argument is the first integer
+The second argument is the second integer
+You have to define a function with this prototype: function add(a, b)
+You must use console.log(...) to print all output
+You are not allowed to use var
+
+
+*/
+
+const a = parseInt(process.argv[2]);
+const b = parseInt(process.argv[3]);
+
+function add(a, b) {
+    return a + b;
+}
+
+console.log(add(a, b));
